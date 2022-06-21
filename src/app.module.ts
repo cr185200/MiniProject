2796e63c9ae49706bd226561c44d9e9b62f90981
NCollinsRakowski <106404185+NCollinsRakowski@users.noreply.github.com>
2022-06-21 14:34:18 -0400

    message

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ApiModule } from './api/api.module';
 import { UtilsModule } from './utils/utils.module';
 import { ConfigModule } from '@nestjs/config';
 import { DomainModule } from './domain/domain.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), //
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     DomainModule,
     ApiModule,
--- src/domain/catalog/catalog.module.ts
+++ src/domain/catalog/catalog.module.ts
@@ -3,9 +3,10 @@ import { CatalogService } from './catalog.service';
 import { CatalogClient } from 'src/utils/catalog/catalogClient';
 import { UtilsModule } from 'src/utils/utils.module';
 import { DomainModule } from '../domain.module';
+import { CatalogResolver } from './catalog.resolver';
 
 @Module({
-  providers: [CatalogService],
+  providers: [CatalogService, CatalogResolver],
   exports: [CatalogService],
   imports: [UtilsModule],
 })
