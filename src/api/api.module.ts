7f143cc2931a7d2d537850a4c76dfe2592579db8
dw185189 <dw185189@ncr.com>
2022-06-22 11:31:52 -0400

    fixed camel-case

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CatalogModule } from './catalog/catalog.module';
-import { ItemAvailabilityModule } from './itemavailability/itemAvailability.module';
+import { ItemAvailabilityModule } from './itemAvailability/itemAvailability.module';
 
 @Module({
   imports: [CatalogModule, ItemAvailabilityModule]
